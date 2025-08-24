53761e4e21d9d8b4669b2b108b2504ef27eca66b
BibekYadav11111 <bibeky135@gmail.com>
2025-08-24 16:13:07 +0545

    Add files via upload

---
+++ lab 3/4.py
@@ -0,0 +1,5 @@
+def info(title,author,year):
+    print(f"title = {title} \nauthor = {author} \nyear = {year}")
+info(title='the lord of rings ',author='J.k rowling',year='2014')
+info(author='J.k rowling',title='the lord of rings ',year='2014')
+info(year='2014',title='the lord of rings ',author='J.k rowling')
